e91ee08f93cf02b07574bf4f3bc9629ea4905177
magnusellemose <magnusellemose@gmail.com>
2025-01-20 11:09:20 +0200

    Added documentation

--- v1/add.go
+++ v1/add.go
@@ -1,5 +1,11 @@
 package add
 
+// Add adds two numbers
+//
+// It has two parameters: num1 and num2.
+// It adds the two parameters and returns the result
+//
+// More information on addition see [https://www.mathsisfun.com/numbers/addition.html]
 func Add(num1, num2 int) int {
 	return num1 + num2
 }
